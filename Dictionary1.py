4185f746ee8da037464cfc555a3d4592267ad983
Deven Anil Borse <devenborse37@gmail.com>
2026-08-25 15:28:49 +0530

    Dictionary

---
+++ Dictionary1.py
@@ -0,0 +1,11 @@
+# dictionary is a type of data that contains key, value pair.
+
+# creating a dictionary
+
+dict_marks = {'subject':'Maths ', 'marks': 90}
+print(dict_marks)  # output: {'subject': 'Maths ', 'marks': 90}
+
+
+dict_marks = {'subject':['Maths ','Eng','Marathi'], 'marks': [90,80,86,52]}
+print(dict_marks)
+
