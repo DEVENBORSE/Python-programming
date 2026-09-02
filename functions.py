31d4fa650a104ba3b6616edf9c5ea9f4ae79e039
Deven Anil Borse <devenborse37@gmail.com>
2026-09-02 21:07:02 +0530

    The code of Function usage

---
+++ functions.py
@@ -0,0 +1,29 @@
+
+# def avg():
+#     a = int(input("enter the number: "))
+#     b = int(input("enter the number: "))
+#     c = int(input("enter the number: "))
+
+#     average = (a+b+c)/3
+#     print(average)
+
+# avg()
+
+
+# def greet(name, ending):
+#     print("have a good day, " + name)
+#     print(ending)
+# greet("Deven", "Thankyou")
+# greet("Rohan", "Thankyou")
+# greet("Dev", "Thankyou")
+
+
+# def factorial(n):
+#     if ( n==1 or n==0):
+#         return 1
+#     return n * factorial(n-1)
+
+# n = int(input("enter the number: "))
+# print(f"the factorial of this number is: {factorial(n)}")
+
+
